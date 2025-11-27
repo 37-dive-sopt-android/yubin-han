e0c64c99db0dd93321775243d68f8b4987a07071
oilbeaneda <20221444@sungshin.ac.kr>
2025-11-27 15:53:29 +0900

    del/중복 파일 삭제

--- app/src/main/java/com/sopt/dive/domain/model/UserModels.kt
+++ app/src/main/java/com/sopt/dive/domain/model/UserModels.kt
@@ -6,3 +6,11 @@ data class ProfileSummary(
     val profileImageResId: Int
 )
 
+data class UserModel(
+    val id: Long,
+    val username: String,
+    val name: String,
+    val email: String,
+    val age: Int,
+    val profileImageUrl: String?
+)
